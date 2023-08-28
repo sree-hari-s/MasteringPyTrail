f32ed61214dc0001ae7960632f8bf87d946413a3
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-08-29 02:03:59 +0530

    Day 1 tasks added

---
+++ Day-1/day-1-printing-start.py
@@ -0,0 +1,19 @@
+#Basic String Manipulation
+print('Hello world')
+
+#Concatenations
+print('hello'+'guys')
+
+#Adding spaces between
+print('hello'+' ' +'guys')
+
+#String to next line
+print("hi guys\nGood Morning")
+
+#Input from User
+#Then Print will show "hello" and user input
+print("Hello "+ input("What is your name?"))
+
+#length of your name
+print(len(input("What is your name?")))
+
