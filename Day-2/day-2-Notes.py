6542b845bcd14949e3128d425dc07884b60e8231
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-08-30 00:08:36 +0530

    Day 2 started

---
+++ Day-2/day-2-Notes.py
@@ -0,0 +1,28 @@
+#Datatypes
+
+#string
+print("hello"[0]) #returns a first character
+"""
+pulling out a character from a string is called subscripting
+number in between square brackets determines which character you pull out
+"""
+print("hello"[-1]) #returns the last character
+
+print("123"+"345") #concatenates to 123345
+
+#Integer
+
+print(123+345) #returns the sum 
+
+#float 
+"""
+refers to floating point numbers, to represent pi 
+"""
+
+pi = 3.14
+
+#boolean
+True , False
+
+print(True+False)
+print(False+False)
