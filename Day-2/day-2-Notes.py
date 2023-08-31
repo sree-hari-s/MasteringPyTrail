975c7ed1a01852c2be52ac770550f9e52590816e
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-08-31 20:37:39 +0530

    day 2 completed

--- Day-2/day-2-Notes.py
+++ Day-2/day-2-Notes.py
@@ -26,3 +26,25 @@ True , False
 
 print(True+False)
 print(False+False)
+
+
+#Type Error
+
+num_char = len(input("What is your name? "))
+"""
+Type Error- cannot concatenate integer into a string
+"""
+#print("Your name is " + num_char+ "characters long")
+
+new_num_char = str(num_char)
+print("Your name is " + new_num_char+ " characters long")
+
+print(type(num_char))
+print(type(new_num_char))
+
+a=123
+print(type(a))
+
+print(10 +float("100.0")) #110.0 output
+print(str(10)+ str(100)) #10100 output
+
