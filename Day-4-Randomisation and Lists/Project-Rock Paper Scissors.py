2a4c3c449741bb470575f018a455a39376237662
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-03 23:21:41 +0530

    day 4 completed

---
+++ Day-4-Randomisation and Lists/Project-Rock Paper Scissors.py
@@ -0,0 +1,68 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+your_choice = int(input("What do you choose? Type 0 for Rock,1 for Paper and 2 for Scissors.\n"))
+computer_choice = random.randint(0,2)
+if your_choice == 0:
+    print(rock)
+elif your_choice == 1:
+    print(paper)
+elif your_choice == 2:
+    print(scissors)
+else:
+    print("Invalid choice!")
+print("Computer chose:")   
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+elif computer_choice == 2:
+    print(scissors)
+
+if your_choice == computer_choice:
+    print("Draw!")
+elif (your_choice == 0 and computer_choice == 1) or (your_choice == 1 and computer_choice == 2) or (your_choice ==2 and computer_choice == 0) :
+    print("You Lose")
+elif (your_choice == 0 and computer_choice == 2) or (your_choice == 1 and computer_choice == 0) or (your_choice == 2 and computer_choice ==1):
+    print("You Win")
+
+    #Draw 
+    #0 0
+    #1 1
+    #2 2     
+    
+    # Lose
+    # 0 1 
+    # 1 2
+    # 2 0 
+    
+    # Win 
+    # 0 2 
+    # 1 0 
+    # 2 1
--- Day-4-Randomisation and Lists/day-4-Notes.py
+++ Day-4-Randomisation and Lists/day-4-Notes.py
@@ -1,31 +1,52 @@
-import random
-a=random.randint(1,2)
-print(a)
+# import random
+# a=random.randint(1,2)
+# print(a)
 
-b=random.random()
-print(b)
+# b=random.random()
+# print(b)
 
-alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
-                'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z']
+# alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+#                 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z']
 
-#list elements can be accessed by positive indexing and negative indexing also
-print(alphabets[0])
+# #list elements can be accessed by positive indexing and negative indexing also
+# print(alphabets[0])
 
-print(alphabets[-1])
+# print(alphabets[-1])
 
-#add values to a list
+# #add values to a list
 
-alphabets.append("AA")
+# alphabets.append("AA")
 
-alphabets.extend(["AB","AC","AD","AE","AF"])
+# alphabets.extend(["AB","AC","AD","AE","AF"])
 
-print(alphabets)
+# print(alphabets)
 
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 
-dirty_dozen = [fruits, vegetables]
+# dirty_dozen = [fruits, vegetables]
 
-print(dirty_dozen[1][1])
-print(dirty_dozen[0])
-print(dirty_dozen[1])
+# print(dirty_dozen[1][1])
+# print(dirty_dozen[0])
+# print(dirty_dozen[1])
+
+
+# # 🚨 Don't change the code below 👇
+# row1 = ["⬜️","️⬜️","️⬜️"]
+# row2 = ["⬜️","⬜️","️⬜️"]
+# row3 = ["⬜️️","⬜️️","⬜️️"]
+# map = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}")
+# position = input("Where do you want to put the treasure? ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this row 👇
+# first_digit = int(position[0])
+# second_digit = int(position[1])
+# map[second_digit-1][first_digit-1] = 'X'
+
+# #Write your code above this row 👆
+
+# # 🚨 Don't change the code below 👇
+# print(f"{row1}\n{row2}\n{row3}")
+
