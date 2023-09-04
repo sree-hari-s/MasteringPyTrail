f2d147ad0b732f153733b3f061b13a65efed10f4
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-05 00:19:29 +0530

    Day 5 completed

---
+++ Day-5-Python Loops/Task4-Fizzbuzz.py
@@ -0,0 +1,44 @@
+"""
+You are going to write a program that automatically prints the solution to the FizzBuzz game.
+
+Your program should print each number from 1 to 100 in turn.
+
+When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+
+When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+
+And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+
+e.g. it might start off like this:
+
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+.... etc.
+
+Hint
+Remember your answer should start from 1 and go up to and including 100.
+2. Each number/text should be printed on a separate line.
+"""
+
+for num in range(1,101):
+    if num%3 == 0 and num%5 == 0:
+        print("FizzBuzz")
+    elif num%3 == 0:
+        print("Fizz")
+    elif num%5 == 0:
+        print("Buzz")
+    else:
+        print(num)
--- Day-5-Python Loops/day-5-Notes.py
+++ Day-5-Python Loops/day-5-Notes.py
@@ -1,6 +1,30 @@
-#For loop
-alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
-             "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# #For loop
+# alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+#              "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-for alphabet in alphabets:
-    print(alphabet)
+# for alphabet in alphabets:
+#     print(alphabet)
+    
+# total =0
+# for i in range(1, 101):
+#     total += i
+# print(total)
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+not_randomized_password = ""
+for _ in range(nr_letters):
+    not_randomized_password+=random.choice(letters)
+for _ in range(nr_symbols):
+    not_randomized_password+=random.choice(symbols)
+for _ in range(nr_numbers):
+    not_randomized_password+=random.choice(numbers)
+
+print(not_randomized_password)
