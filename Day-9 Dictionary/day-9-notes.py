30ece4611d848f7a21d369e5cfaca4e450902e5e
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-10 00:14:05 +0530

    day-9 started

---
+++ Day-9 Dictionary/day-9-notes.py
@@ -0,0 +1,8 @@
+programming_dictionary={
+    "bug":"An error in a program",
+    "function": "A piece of code that you can call easily over and over again"
+}
+
+programming_dictionary["loop"] ="The action of doing something over and over again"
+
+print(programming_dictionary)
