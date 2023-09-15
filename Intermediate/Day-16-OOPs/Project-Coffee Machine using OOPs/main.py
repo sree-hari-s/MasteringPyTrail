6f32c19ad9c85f9611fad9f08aba63d81f135be0
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-15 23:19:15 +0530

    completed using oops

--- Intermediate/Day-16-OOPs/Project-Coffee Machine using OOPs/main.py
+++ Intermediate/Day-16-OOPs/Project-Coffee Machine using OOPs/main.py
@@ -1,3 +1,23 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+
+
+is_on = True
+while is_on:
+    choice = input(f"What would you like to order? ({menu.get_items()}) ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
