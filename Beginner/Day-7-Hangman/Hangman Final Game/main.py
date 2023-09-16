687f3d0733f9ce027fb1ed5338ce4781127c9d59
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-16 13:28:47 +0530

    bugs fixed and while loop added

--- Beginner/Day-7-Hangman/Hangman Final Game/main.py
+++ Beginner/Day-7-Hangman/Hangman Final Game/main.py
@@ -2,42 +2,49 @@ from hangman_words import word_list
 from hangman_art import logo,stages
 import random
 
-print(logo)
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
 
-end_of_game = False
-lives = 6
+def game():
+    print(logo)
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
 
-#Create blanks
-display = []
-for _ in range(word_length):
-    display += "_"
+    end_of_game = False
+    lives = 6
 
-while not end_of_game:
-    guess = input("Guess a letter: ").lower()
+    #Create blanks
+    display = []
+    for _ in range(word_length):
+        display += "_"
+    while not end_of_game:
+        guess = input("Guess a letter: ").lower()
 
-    #Check guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-            display[position] = letter
+        #Check guessed letter
+        for position in range(word_length):
+            letter = chosen_word[position]
+            #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter
 
-    #Check if user is wrong.
-    if guess not in chosen_word:
-        lives -= 1
-        print(f"You guessed {guess}, that's not in the word,You lose a life({lives} remaining)")
-        if lives == 0:
+        #Check if user is wrong.
+        if guess not in chosen_word:
+            lives -= 1
+            print(f"You guessed {guess}, that's not in the word,You lose a life({lives} remaining)")
+            if lives == 0:
+                end_of_game = True
+                print(f"You lose. The word was {chosen_word}")
+
+        #Join all the elements in the list and turn it into a String.
+        print(f"{' '.join(display)}")
+
+        #Check if user has got all letters.
+        if "_" not in display:
             end_of_game = True
-            print("You lose.")
+            print("You win.")
+            
+        print(stages[lives])
 
-    #Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
+from os import system
 
-    #Check if user has got all letters.
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
-        
-    print(stages[lives])
+while input("Do you want to play Hangman Game? (y/n): ") == "y":
+    system('cls')
+    game()
