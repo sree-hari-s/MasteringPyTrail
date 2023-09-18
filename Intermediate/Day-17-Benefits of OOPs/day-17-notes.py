782f8f83f5259a9ff0e1e4bee7c4087b27af3279
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-18 16:56:33 +0530

    adding method to the class

--- Intermediate/Day-17-Benefits of OOPs/day-17-notes.py
+++ Intermediate/Day-17-Benefits of OOPs/day-17-notes.py
@@ -4,15 +4,22 @@ class User:
         self.id = id
         self.name = name
         self.followers = 0
+        self.following = 0
+        
+    def follow(self,user):
+        user.following += 1
+        self.following += 1
 
 user_1 = User('001','sreehari')
 user_2 = User('002','sreerag')
 print(user_1.followers)
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_2.followers)
 
-
-class Car:
-    def __init__(self,seats):
-        self.seats = seats
+# class Car:
+#     def __init__(self,seats):
+#         self.seats = seats
         
-my_car = Car(5)
-print(my_car.seats)
+# my_car = Car(5)
+# print(my_car.seats)
