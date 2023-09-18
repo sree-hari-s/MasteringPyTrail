8122ac6d9eff2b9256fb187a9e7ca61c5add1fd7
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-18 21:05:36 +0530

    basic project completed

--- Intermediate/Day-17-Benefits of OOPs/Project-Quiz App/main.py
+++ Intermediate/Day-17-Benefits of OOPs/Project-Quiz App/main.py
@@ -9,6 +9,14 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-quiz = QuizBrain(question_bank)
-while quiz.still_has_question():
-    quiz.next_question()
+def game():
+    quiz = QuizBrain(question_bank)
+    while quiz.still_has_question():
+        quiz.next_question()
+    print("Quiz Completed!")
+    print(f"Your final score was {quiz.score}/{len(quiz.question_list)}\n")
+
+from os import system
+while input("Do you want to play a Quiz Game? (y/n): ") =='y':
+    system('cls')
+    game()
