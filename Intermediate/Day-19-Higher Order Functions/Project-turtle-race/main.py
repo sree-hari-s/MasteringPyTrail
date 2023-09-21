8f4d9c75d6b83a22177012c5aa62578ebd87d988
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-22 02:12:09 +0530

    positions adjusted

--- Intermediate/Day-19-Higher Order Functions/Project-turtle-race/main.py
+++ Intermediate/Day-19-Higher Order Functions/Project-turtle-race/main.py
@@ -5,8 +5,8 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput("Make Your Bet","Which turtle will win the race: ? Enter a color? ").lower()
 
 colors =['violet','indigo','blue','green','yellow','orange','red']
-y_start = [100,60,20,-20,-60,-100,-140,-180]
-for i in range(len(colors)-1):
+y_start = [180,120,60,0,-60,-120,-180]
+for i in range(len(colors)):
     tim = Turtle(shape='turtle')
     tim.color(colors[i])
     tim.penup()
