f74166d3f6773eff4b80695c3ec93fc5c9fee9b8
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-21 23:56:06 +0530

    Update Task3-Shapes.py

--- Intermediate/Day-18-Turtle & GUI/Task3-Shapes.py
+++ Intermediate/Day-18-Turtle & GUI/Task3-Shapes.py
@@ -6,8 +6,8 @@ screen = Screen()
 side = 3
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 while side <= 10:
+    pointer.color(random.choice(colors))
     for _ in range(side):
-        pointer.pencolor(random.choice(colors))
         pointer.pensize(2)
         pointer.forward(50)
         pointer.left(360/side)
