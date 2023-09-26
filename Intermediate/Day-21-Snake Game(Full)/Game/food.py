707635b9e52cde03deb3378c1d14e98469aba6b0
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-27 00:53:57 +0530

    food icon changed

--- Intermediate/Day-21-Snake Game(Full)/Game/food.py
+++ Intermediate/Day-21-Snake Game(Full)/Game/food.py
@@ -4,7 +4,7 @@ from random import randint
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid = 0.5)
         self.color("red")
