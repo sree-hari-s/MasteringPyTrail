1a38aa016b46c5f0a1dc8c5473c70b7ab6ad9822
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-27 00:53:50 +0530

    slicing implemented

--- Intermediate/Day-21-Snake Game(Full)/Game/main.py
+++ Intermediate/Day-21-Snake Game(Full)/Game/main.py
@@ -38,10 +38,8 @@ while game_is_on:
         score.game_over()
         
     #Detect the collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment)<10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment)<10:
             game_is_on = False
             score.game_over()
             
