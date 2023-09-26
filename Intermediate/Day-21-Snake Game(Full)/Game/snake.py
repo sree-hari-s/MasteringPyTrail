f794d2202b796eb838415442903be3b1b6128374
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-27 01:02:47 +0530

    snake head changed

--- Intermediate/Day-21-Snake Game(Full)/Game/snake.py
+++ Intermediate/Day-21-Snake Game(Full)/Game/snake.py
@@ -12,6 +12,7 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+        self.head.shape("triangle")
 
     def create_snake(self):
         for position in STARTING_POSITION:
