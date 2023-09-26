93b1bd99bf836545f5ac3d0ce9e5b5d74ddffae7
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-27 01:25:38 +0530

    repeat game and screen color fixed

--- Intermediate/Day-21-Snake Game(Full)/Game/main.py
+++ Intermediate/Day-21-Snake Game(Full)/Game/main.py
@@ -4,43 +4,54 @@ from snake import Snake
 from food import Food
 from score import Score
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
 
-snake = Snake()
-food = Food()
-score = Score()
+def Screen_setup():
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
 
-screen.listen()
-screen.onkey(snake.up,'Up')
-screen.onkey(snake.down,'Down')
-screen.onkey(snake.left,'Left')
-screen.onkey(snake.right,'Right')
+def game():
+    Screen_setup()
+    screen.title("Snake Game")
+    screen.tracer(0)
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    snake = Snake()
+    food = Food()
+    score = Score()
 
-    #Detect the collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh_food_location()
-        score.increase_score()
-        snake.extend()
-        
-    #Detect the collision with wall
-    if snake.head.xcor() >280 or snake.head.xcor() <-280 or snake.head.ycor() > 280 or snake.head.ycor() <-280:
-        game_is_on = False
-        score.game_over()
-        
-    #Detect the collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment)<10:
+    screen.listen()
+    screen.onkey(snake.up,'Up')
+    screen.onkey(snake.down,'Down')
+    screen.onkey(snake.left,'Left')
+    screen.onkey(snake.right,'Right')
+
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+        #Detect the collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh_food_location()
+            score.increase_score()
+            snake.extend()
+            
+        #Detect the collision with wall
+        if snake.head.xcor() >280 or snake.head.xcor() <-280 or snake.head.ycor() > 280 or snake.head.ycor() <-280:
             game_is_on = False
             score.game_over()
             
-screen.exitonclick()
+        #Detect the collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment)<10:
+                game_is_on = False
+                score.game_over()
+
+screen = Screen()
+Screen_setup()
+
+while screen.textinput("Snake Game", "Do you want to play Snake Game? y/n:").lower() == "y":
+    game()
+    time.sleep(2)
+    screen.clearscreen()
+    Screen_setup()
