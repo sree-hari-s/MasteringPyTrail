dbd785ac7f3c6c796949cfe5c53f1377370b5170
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-28 20:40:43 +0530

    paddle updated,score setup completed

--- Intermediate/Day-22-Pong-Game/paddle.py
+++ Intermediate/Day-22-Pong-Game/paddle.py
@@ -1,13 +1,13 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self,pos_x,pos_y):
+    def __init__(self,position):
         super().__init__()
         self.shape('square')
         self.color('white')
         self.shapesize(stretch_wid=5,stretch_len=1)
         self.penup()
-        self.goto(x=pos_x,y=pos_y)
+        self.goto(position)
     
     def go_up(self):
         new_y = self.ycor()+20
