cc03bbfee783c74802303328acfaf9ef38b247b0
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-28 20:43:52 +0530

    base game completed

--- Intermediate/Day-22-Pong-Game/main.py
+++ Intermediate/Day-22-Pong-Game/main.py
@@ -1,36 +1,58 @@
-from turtle import Screen
-import time
+from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from score import Score
+import time
+
 
 def Screen_setup():
-    screen.bgcolor('black')
-    screen.setup(width=800,height=600)
+    screen.bgcolor("black")
+    screen.setup(width=800, height=600)
+    screen.title("Pong")
+    screen.tracer(0)
 
 def game():
     Screen_setup()
-    screen.tracer(0)
-    
-    r_paddle = Paddle(-350,0)
-    l_paddle = Paddle(350,0)
-    
+    r_paddle = Paddle((350, 0))
+    l_paddle = Paddle((-350, 0))
+    ball = Ball()
+    score = Score()
+
     screen.listen()
-    screen.onkey(r_paddle.go_up,'w')
-    screen.onkey(r_paddle.go_down,'s')
-    screen.onkey(l_paddle.go_up,'Up')
-    screen.onkey(l_paddle.go_down,'Down')
-    
+    screen.onkey(r_paddle.go_up, "Up")
+    screen.onkey(r_paddle.go_down, "Down")
+    screen.onkey(l_paddle.go_up, "w")
+    screen.onkey(l_paddle.go_down, "s")
+
     game_is_on = True
     while game_is_on:
         screen.update()
+        time.sleep(0.05)
+        ball.move()
 
+        #Detect collision with wall
+        if ball.ycor() > 280 or ball.ycor() < -280:
+            ball.bounce_y()
+
+        #Detect collision with paddle
+        if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+            ball.bounce_x()
+
+        #Detect R paddle misses
+        if ball.xcor() > 380:
+            ball.reset_position()
+            score.l_point()
+
+        #Detect L paddle misses:
+        if ball.xcor() < -380:
+            ball.reset_position()
+            score.r_point()
+            
 screen = Screen()
 Screen_setup()
-
 while screen.textinput("Ping-Pong Game", "Do you want to play Pong Game? y/n:").lower() == "y":
     game()
     time.sleep(2)
     screen.clearscreen()
     Screen_setup()
-
-
 screen.exitonclick()
