d4560be28313bc405e7f9ffdf2cc7671e5c82940
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-29 20:46:34 +0530

    basic score board setup done

--- Intermediate/Day-23-Capstone Project1/main.py
+++ Intermediate/Day-23-Capstone Project1/main.py
@@ -8,6 +8,8 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+score = Scoreboard()
+
 player = Player()
 screen.listen()
 screen.onkey(player.move_up,'Up')
--- Intermediate/Day-23-Capstone Project1/scoreboard.py
+++ Intermediate/Day-23-Capstone Project1/scoreboard.py
@@ -1,5 +1,20 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('black')
+        self.penup()
+        self.hideturtle()
+        self.goto(-280,260)
+        self.level = 1
+        self.current_level()
+        
+    def increase_level(self):
+        self.level += 1
+        self.current_level()
+        
+    def current_level(self):
+        self.write(f'Level {self.level}',font=FONT)
