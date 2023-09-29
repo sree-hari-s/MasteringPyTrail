6ccc71e56e79e41aa23650f3c8f8af77e9d7bc97
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-29 20:40:47 +0530

    basic turtle and movement setup done

--- Intermediate/Day-23-Capstone Project1/main.py
+++ Intermediate/Day-23-Capstone Project1/main.py
@@ -8,6 +8,10 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+screen.listen()
+screen.onkey(player.move_up,'Up')
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- Intermediate/Day-23-Capstone Project1/player.py
+++ Intermediate/Day-23-Capstone Project1/player.py
@@ -1,7 +1,17 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.color('black')
+        self.penup()
+        self.left(90)
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
