d494c86b4e46d2384b60b97a4e78a9e12f72f224
Agniva Shil <81551351+Agniva144@users.noreply.github.com>
2023-10-02 19:05:03 +0530

    Add weather App in python

---
+++ Projects/Weather App/app.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import messagebox
+import requests
+
+# Replace this with your WeatherAPI.com RapidAPI key
+RAPIDAPI_KEY = "YOUR_RAPIDAPI_KEY"
+
+def get_weather(city):
+    url = "https://weatherapi-com.p.rapidapi.com/current.json"
+    querystring = {"q": city}
+    headers = {
+        "X-RapidAPI-Key": "5f905f3077msh04f80a45333d781p13b9c0jsnfa4ea4e7144a",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    if response.status_code == 200:
+        data = response.json()
+        location = data['location']['name']
+        temperature = data['current']['temp_c']
+        weather_description = data['current']['condition']['text']
+
+        result_label.config(text=f'Weather in {location}:\nTemperature: {temperature}°C\nDescription: {weather_description}')
+    else:
+        messagebox.showerror('Error', 'City not found.')
+
+def display_weather():
+    city = city_entry.get()
+    if not city:
+        messagebox.showerror('Error', 'Please enter a city name.')
+        return
+
+    get_weather(city)
+
+# Create the main window
+window = tk.Tk()
+window.title('Weather App')
+
+# Create and arrange widgets
+city_label = tk.Label(window, text='Enter City:')
+city_label.pack()
+
+city_entry = tk.Entry(window)
+city_entry.pack()
+
+get_weather_button = tk.Button(window, text='Get Weather', command=display_weather)
+get_weather_button.pack()
+
+result_label = tk.Label(window, text='', wraplength=300)
+result_label.pack()
+
+# Start the GUI main loop
+window.mainloop()
