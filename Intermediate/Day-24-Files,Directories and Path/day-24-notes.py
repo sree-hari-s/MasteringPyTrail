dbdce8807b3de5aa31be7c401e25c4a510efcd7f
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-03 23:50:19 +0530

    using read and write

---
+++ Intermediate/Day-24-Files,Directories and Path/day-24-notes.py
@@ -0,0 +1,26 @@
+file = open('text_file.txt')
+contents = file.read()
+print(contents)
+file.close()
+
+"""
+closes as soon as the task is completed
+"""
+with open('text_file.txt') as file:
+    contents = file.read()
+    print(contents)
+    
+"""
+mode `a` appends data to the existing file keeping the previous data also
+"""
+with open('text_file.txt',mode="a") as file:
+    file.write("\nI am currently learning Python")
+    
+"""
+if a file does not exist python creates a new file in the same directory
+with the name provided 
+Only works in the write mode and when file does not exist
+"""
+
+with open('new_file.txt',mode="w") as file:
+    file.write("New file created")
