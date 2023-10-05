d0ca3b8a6f2787304337c98023ef31a7c485c80b
Shovit <119912892+Virtual4087@users.noreply.github.com>
2023-10-05 17:20:18 +0545

    Update settings.py

--- Projects/Django-Projects/rain_detect/rain_detect/settings.py
+++ Projects/Django-Projects/rain_detect/rain_detect/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x-z0f1&dp073cfsb2qwjkmagx6-6&)dqewt=$gqxmi&vdni)5='
+SECRET_KEY = ${{ secrets.Django_Key }}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
