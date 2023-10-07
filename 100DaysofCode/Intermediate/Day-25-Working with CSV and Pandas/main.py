c3a37b9d6298c9dedc200323ff4746fa88dc5230
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-07 21:12:01 +0530

    day 25 started

---
+++ 100DaysofCode/Intermediate/Day-25-Working with CSV and Pandas/main.py
@@ -0,0 +1,36 @@
+# with open('weather_data.csv', 'r') as file:
+#     data=file.readlines()
+    
+# print(data)
+
+import csv
+
+with open('weather_data.csv', 'r') as data_file:
+    data= csv.reader(data_file)
+    temperature = []
+    for row in data:
+        if row[1]!='temp':
+            temperature.append(int(row[1]))
+    print(temperature)
+    
+"""
+Pandas library
+"""
+
+import pandas as pd
+
+new_data = pd.read_csv('weather_data.csv')
+print(type(new_data))
+print(new_data['temp'])
+
+"""
+2 type of datatypes in Pandas
+Data frame and Series
+"""
+
+data_dict = new_data.to_dict()
+print(data_dict)
+
+temp_list = new_data['temp'].to_list()
+print(len(temp_list))
+print(sum(temp_list)/len(temp_list))
