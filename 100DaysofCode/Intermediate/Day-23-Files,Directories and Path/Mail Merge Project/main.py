6c506beaf71edeaf62fb35b4e8505931b638cd38
avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
2023-10-08 22:32:57 +0300

    new pull request (#36)
    * Aim Trainer
    
    This is an easy to understand Tkinter aim trainer.
    This project was written so it would be easy for beginners\intermediates to understand python and especially the Tkinter library.
    
    * added README and fixed days
    
    moved my project to the Tkinter folder and added REAME with brief description of my work
    
    * Delete .idea/modules.xml
    
    * Delete .idea/vcs.xml
    
    * Delete .idea/.gitignore
    
    * Delete .idea/100DaysOfCode-Python.iml
    
    * Delete .idea/discord.xml
    
    * Delete .idea/inspectionProfiles/profiles_settings.xml
    
    * Delete .idea/misc.xml
    
    * not required
    
    * modified How to run
    
    * Update README.md
    
    ---------
    
    Co-authored-by: SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>

--- 100DaysofCode/Intermediate/Day-23-Files,Directories and Path/Mail Merge Project/main.py
+++ 100DaysofCode/Intermediate/Day-23-Files,Directories and Path/Mail Merge Project/main.py
@@ -2,7 +2,6 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-
 PLACEHOLDER = '[name]'
 
 with open('./Input/Names/invited_names.txt', 'r') as name_file:
