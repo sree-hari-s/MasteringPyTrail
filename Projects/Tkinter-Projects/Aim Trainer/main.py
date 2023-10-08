b3fba5beb91d50986a457a766ea6635e3d6c87ad
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-09 01:00:59 +0530

    Squashed commit of the following:
    commit 79c48c56484f10be34cc320a40067d7b86a64d67
    Merge: ae52d6a dd637b1
    Author: SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
    Date:   Mon Oct 9 00:57:45 2023 +0530
    
        Merge branch 'main' of https://github.com/avimoti1234/MasteringPyTrail into test
    
    commit dd637b1b853cf02dcd828f779c9ca2475bc3f096
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Sun Oct 8 20:49:08 2023 +0300
    
        Update README.md
    
    commit 69491bedb9505f34e753aaba85b2ffff3583af1a
    Author: SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
    Date:   Sun Oct 8 05:43:42 2023 +0530
    
        modified How to run
    
    commit 37b3204b6f4910659477b5259673d054769de322
    Author: SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
    Date:   Sun Oct 8 05:43:22 2023 +0530
    
        not required
    
    commit e9fc2eabbf245e97485c4f9beddb74349bc318b8
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:27:02 2023 +0300
    
        Delete .idea/misc.xml
    
    commit bcf2558339580c5c433875a4e5bf4eb3776a807f
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:26:50 2023 +0300
    
        Delete .idea/inspectionProfiles/profiles_settings.xml
    
    commit 9396074beda9a9e218a4787d79c470b597e0fa63
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:26:21 2023 +0300
    
        Delete .idea/discord.xml
    
    commit 22f5d20219d2f79c859f0a5c1c8b7925950aaaf2
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:26:09 2023 +0300
    
        Delete .idea/100DaysOfCode-Python.iml
    
    commit 071f99c544fe9480fa28769c3ef6149f85943268
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:25:51 2023 +0300
    
        Delete .idea/.gitignore
    
    commit 3973de98c3e459d88b3bdfe6116e57b951eac7d4
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:25:38 2023 +0300
    
        Delete .idea/vcs.xml
    
    commit ee159f161411142a35e7417e15328faa64472c12
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:25:25 2023 +0300
    
        Delete .idea/modules.xml
    
    commit cd49e7e3d2a2de0e9c226e49a51b7ab731e0438b
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 22:20:16 2023 +0300
    
        added README and fixed days
    
        moved my project to the Tkinter folder and added REAME with brief description of my work
    
    commit 4fb095d11c335e4f6359996ffbc382db1f529231
    Author: avimoti1234 <73740050+avimoti1234@users.noreply.github.com>
    Date:   Fri Oct 6 15:45:40 2023 +0300
    
        Aim Trainer
    
        This is an easy to understand Tkinter aim trainer.
        This project was written so it would be easy for beginners\intermediates to understand python and especially the Tkinter library.

---
+++ Projects/Tkinter-Projects/Aim Trainer/main.py
@@ -0,0 +1,52 @@
+import tkinter
+import random
+
+TARGET_RADIUS = 30
+X, Y = 340, 30
+
+
+class MainWindows:
+    def __init__(self, width, height, title, target_color):
+        self.width = width
+        self.height = height
+        self.size = self.width if self.width < self.height else self.height
+        self.title = title
+        self.target_color = target_color
+        self.success_counter = 0
+
+        root = tkinter.Tk()
+        root.title(self.title)
+        root.resizable(False, False)
+
+        self.canvas = tkinter.Canvas(root, width=self.width, height=self.height)
+        self.canvas.pack()
+
+        self.draw_target()
+
+    def draw_target(self):
+        self.text = self.canvas.create_text(X, Y, text=f"score: {self.success_counter}", font=("Helvetica", 16),
+                                            fill="black")
+
+        x = random.randint(self.size * (TARGET_RADIUS / self.size), self.size * (1 - TARGET_RADIUS / self.size))
+        y = random.randint(self.size * (TARGET_RADIUS / self.size), self.size * (1 - TARGET_RADIUS / self.size))
+        x0 = x - TARGET_RADIUS
+        y0 = y - TARGET_RADIUS
+        x1 = x + TARGET_RADIUS
+        y1 = y + TARGET_RADIUS
+
+        self.target = self.canvas.create_arc(x0, y0, x1, y1, extent=359, start=0, fill=self.target_color,
+                                             outline=self.target_color)
+
+        self.canvas.tag_bind(self.target, "<Button-1>", self.on_click)
+
+    def on_click(self, event):
+        self.canvas.delete(self.target)
+        self.canvas.delete(self.text)
+        self.success_counter += 1
+        self.draw_target()
+
+    def run(self):
+        self.canvas.mainloop()
+
+
+MainWindows(700, 600, "Aim Trainer", "red").run()
