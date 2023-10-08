3cc32db77c8a58b8e91c7317d514060e334a1b7c
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-09 01:47:22 +0530

    task3 starting file

---
+++ 100DaysofCode/Intermediate/Day-26-List,Dict-Comprehension/Task3-Data Overlap/main.py
@@ -0,0 +1,7 @@
+# TODO
+"""
+Take a look inside file1.txt and file2.txt. They each contain a bunch of numbers, each number on a new line.
+
+You are going to create a list called result which contains the numbers that are common in both files.
+"""
+
