fe71149e6ae6b3463b296989e021b483678fbafe
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-08 06:42:58 +0530

    notes completed

--- 100DaysofCode/Intermediate/Day-25-Working with CSV and Pandas/main.py
+++ 100DaysofCode/Intermediate/Day-25-Working with CSV and Pandas/main.py
@@ -11,7 +11,7 @@ with open('weather_data.csv', 'r') as data_file:
     for row in data:
         if row[1]!='temp':
             temperature.append(int(row[1]))
-    print(temperature)
+    print('temperature',temperature)
     
 """
 Pandas library
@@ -21,7 +21,7 @@ import pandas as pd
 
 new_data = pd.read_csv('weather_data.csv')
 print(type(new_data))
-print(new_data['temp'])
+print("temperature data using pandas ",new_data['temp'])
 
 """
 2 type of datatypes in Pandas
@@ -32,14 +32,45 @@ Whole table is a Dataframe
 """
 
 data_dict = new_data.to_dict()
-print(data_dict)
+print("Data in dictionary",data_dict)
 
 temp_list = new_data['temp'].to_list()
-print(len(temp_list))
-print(sum(temp_list)/len(temp_list))
-print(new_data['temp'].mean())
-print(new_data['temp'].max())
+print("length of temperature data",len(temp_list))
+print("Average using sum/len",sum(temp_list)/len(temp_list))
+print("Average using mean function",new_data['temp'].mean())
+print("Maximum temperature data",new_data['temp'].max())
 
 # Get data in columns
 
-print(new_data['condition'])
+print("Accessing column data #1\n",new_data['condition'])
+print("Accessing column data #2\n",new_data.condition)
+
+# Get data in Rows
+
+print("Monday ",new_data[new_data.day == 'Monday'])
+
+print(new_data[new_data.temp == new_data.temp.max()])
+
+# TODO: Convert Monday's temperature to Fahrenheit.
+
+def celsius_to_fahrenheit(temp):
+    new_temp = (temp*(9/5)) + 32
+    return new_temp
+
+print("Temperature Monday :",new_data[new_data.day == 'Monday']['temp'][0])
+fah_temp = celsius_to_fahrenheit(new_data[new_data.day == 'Monday'].temp[0])
+
+print("Temperature in fahrenheit :",fah_temp)
+
+
+# Create Dataframe from Scratch
+
+data_dict = {
+    "students" : [ 'sreehari', 'sreerag', 'nandana'],
+    "score" : [78,89,100]
+}
+
+data = pd.DataFrame(data_dict)
+print(data)
+
+data.to_csv("new_data.csv") # create a csv files
