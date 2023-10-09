6538b358c752e65b42a489ae7f48c422abf7cf50
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-09 15:49:36 +0530

    questions added as comment

--- 100DaysofCode/Intermediate/Day-26-List,Dict-Comprehension/Task1-Squaring Numbers.py
+++ 100DaysofCode/Intermediate/Day-26-List,Dict-Comprehension/Task1-Squaring Numbers.py
@@ -1,3 +1,8 @@
+"""
+Instructions
+You are going to write a List Comprehension to create a new list called squared_numbers. 
+This new list should contain every number in the list numbers but each number should be squared.
+"""
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 # 🚨 Do Not Change the code above 👆
 
--- 100DaysofCode/Intermediate/Day-26-List,Dict-Comprehension/Task2-Filtering Even Numbers.py
+++ 100DaysofCode/Intermediate/Day-26-List,Dict-Comprehension/Task2-Filtering Even Numbers.py
@@ -1,3 +1,13 @@
+"""
+Instructions
+In this list comprehension exercise you will practice using list comprehension to filter out the even numbers from a series of numbers.
+
+First, use list comprehension to convert the list_of_strings to a list of integers.
+
+Then use list comprehension again to create a new list called result. This new list should only contain the even numbers from the list numbers.
+
+Again, try to use Python's List Comprehension instead of a Loop.
+"""
 list_of_strings = input().split(',')
 # 🚨 Do  not change the code above
 
