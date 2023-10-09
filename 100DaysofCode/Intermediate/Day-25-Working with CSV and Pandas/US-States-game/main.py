543fe7a6aa34894ba65152f379e920890e0677d5
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-09 12:08:30 +0530

    list comprehension used

--- 100DaysofCode/Intermediate/Day-25-Working with CSV and Pandas/US-States-game/main.py
+++ 100DaysofCode/Intermediate/Day-25-Working with CSV and Pandas/US-States-game/main.py
@@ -1,34 +1,33 @@
-from turtle import Screen,Turtle,textinput
+import turtle
 import pandas as pd
-t = Turtle()
-screen = Screen()
+
+screen = turtle.Screen()
 screen.title("U.S States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-t.shape(image)
-
-def get_mouse_click_coor(x, y):
-    print(x,y)
+turtle.shape(image)
 
 def write_image(x,y,answer_state):
+    t= turtle.Turtle()
     t.hideturtle()
     t.penup()
     t.goto(x,y)
     t.write(f"{answer_state}")
 
 guessed_states = []
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
 
 while len(guessed_states) <=50:
-    answer_state = textinput(f"{len(guessed_states)}/50 State","What's another state name?").title()
+    answer_state = screen.textinput(f"{len(guessed_states)}/50 State","What's another state name?").title()
     print(answer_state)
     # Get data from the csv file
-    data = pd.read_csv("50_states.csv")
-    all_states = data.state.to_list()
     if answer_state == "Exit":
-        missing_state = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_state.append(state)
+        missing_state = [state for state in all_states if state not in guessed_states]
+        # missing_state = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_state.append(state)
         learn_data = pd.DataFrame(missing_state)
         break
     if answer_state in all_states == data.state.to_list():
@@ -37,7 +36,7 @@ while len(guessed_states) <=50:
         guessed_states.append(answer_state)
     else:
         print("Invalid answer")
-    
+
 print("Learn Missing States",learn_data)
 
 screen.exitonclick()
