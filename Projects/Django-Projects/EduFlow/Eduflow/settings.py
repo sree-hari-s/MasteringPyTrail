6e4b99aee5394f660dc057d6637752dcc98f27c6
praveen <cbsrirangapraveen.2004@gmail.com>
2023-10-12 11:44:39 +0530

    hide the secret key

--- Projects/Django-Projects/EduFlow/Eduflow/settings.py
+++ Projects/Django-Projects/EduFlow/Eduflow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j633yo$cmdi$h4#hnkesy0tqzuhuvyp6d$p(iyij4gia=jj@nd'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Projects/Django-Projects/ToDoList/todolist/settings.py
+++ Projects/Django-Projects/ToDoList/todolist/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_+3mlmabgdv5)um1na$8_m%1h@9cmx&a&_ae_i#+hq@5f=poj)'
+SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
