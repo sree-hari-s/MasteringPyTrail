fe74f1d380ae23a8f92821f3a3ad9fd3911e7213
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-15 00:27:54 +0530

    button and entry component used

--- 100DaysofCode/Intermediate/Day-27-Tkinter,GUI Programs/first-GUI.py
+++ 100DaysofCode/Intermediate/Day-27-Tkinter,GUI Programs/first-GUI.py
@@ -2,9 +2,31 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("First GUI")
-window.minsize(width=500,height=300)
+window.minsize(width=500, height=300)
 
-#label 
-my_label = tkinter.Label(text= "1st Label")
+# label
+my_label = tkinter.Label(text="1st Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+bottom_label = tkinter.Label(text="2nd Label", font=("Arial", 24, "bold"))
+bottom_label.pack(side="bottom")
+
+# button
+def button_clicked():
+    my_label.config(text="Button Got Clicked")
+
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
+
+# Entry Component - input field 
+def modify_label():
+    new_text = input.get()
+    bottom_label.config(text=new_text)
+
+input = tkinter.Entry(width=30)
+input.pack()
+
+entry_button = tkinter.Button(text="Entry", command=modify_label)
+entry_button.pack()
+
 window.mainloop()
