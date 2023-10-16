6b15c84f0735a9c3bdf8172773334eb5c024db1b
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-16 21:12:34 +0530

    day 27 notes

--- 100DaysofCode/Intermediate/Day-27-Tkinter,GUI Programs/layout.py
+++ 100DaysofCode/Intermediate/Day-27-Tkinter,GUI Programs/layout.py
@@ -1,37 +1,38 @@
 import tkinter
 """
-window
-label
-button
-used in this basic gui
+pack 
+place 
+grid
+
 """
+
 window = tkinter.Tk()
 window.title("First GUI")
 window.minsize(width=500, height=300)
 
 # label
-my_label = tkinter.Label(text="1st Label", font=("Arial", 24, "bold"))
-my_label.pack()
+first_label = tkinter.Label(text="1st Label", font=("Arial", 24, "bold"))
+first_label.grid(column=0,row=0)
 
-bottom_label = tkinter.Label(text="2nd Label", font=("Arial", 24, "bold"))
-bottom_label.pack(side="bottom")
+second_label = tkinter.Label(text="2nd Label", font=("Arial", 24, "bold"))
+second_label.grid(column=3,row=0)
 
 # button
 def button_clicked():
-    my_label.config(text="Button Got Clicked")
+    first_label.config(text="Button Got Clicked")
 
 button = tkinter.Button(text="Click me", command=button_clicked)
-button.pack()
+button.grid(column=2,row=2)
 
 # Entry Component - input field 
 def modify_label():
     new_text = input.get()
-    bottom_label.config(text=new_text)
+    second_label.config(text=new_text)
 
 input = tkinter.Entry(width=30)
-input.pack()
+
 
 entry_button = tkinter.Button(text="Entry", command=modify_label)
-entry_button.pack()
+entry_button.grid(column=4,row=3)
 
 window.mainloop()
