ae6af85c24af213ec224b57a875d27030666f557
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-16 23:53:27 +0530

    day 28 starting code

---
+++ 100DaysofCode/Intermediate/Day-28-Tkinter,Dynamic Typing/Pomodoro/main.py
@@ -0,0 +1,22 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro App")
+
+window.mainloop()
