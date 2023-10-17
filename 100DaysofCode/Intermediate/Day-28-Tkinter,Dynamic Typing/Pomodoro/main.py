c02379cbd7444d7ab437c5b7a9c3e5b4ae66c8a4
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-17 23:41:34 +0530

    ui setup completed

--- 100DaysofCode/Intermediate/Day-28-Tkinter,Dynamic Typing/Pomodoro/main.py
+++ 100DaysofCode/Intermediate/Day-28-Tkinter,Dynamic Typing/Pomodoro/main.py
@@ -18,5 +18,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro App")
+window.config(padx=100,pady=50,bg=YELLOW)
+canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
+
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=tomato_img)
+canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+canvas.pack()
 
 window.mainloop()
