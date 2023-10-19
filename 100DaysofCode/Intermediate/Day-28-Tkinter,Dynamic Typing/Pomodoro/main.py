6873e3ca1b471fe8dc88ffe6affbe57e751a32d8
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-19 20:37:17 +0530

    UI completed

--- 100DaysofCode/Intermediate/Day-28-Tkinter,Dynamic Typing/Pomodoro/main.py
+++ 100DaysofCode/Intermediate/Day-28-Tkinter,Dynamic Typing/Pomodoro/main.py
@@ -21,9 +21,21 @@ window.title("Pomodoro App")
 window.config(padx=100,pady=50,bg=YELLOW)
 canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
 
+heading_label = Label(text="Timer",bg=YELLOW,fg=GREEN,font=(FONT_NAME,35,"bold"))
+heading_label.grid(column=2,row=1)
+
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=tomato_img)
 canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.grid(column=2,row=2)
+
+start_button = Button(text="Start",highlightthickness=0)
+start_button.grid(column=1,row=3)
+
+stop_button = Button(text="Stop",highlightthickness=0)
+stop_button.grid(column=3,row=3)
+
+check_mark = Label(text="✔",bg=YELLOW,fg=GREEN,font=(FONT_NAME,35,"bold"))
+check_mark.grid(column=2,row=4)
 
 window.mainloop()
