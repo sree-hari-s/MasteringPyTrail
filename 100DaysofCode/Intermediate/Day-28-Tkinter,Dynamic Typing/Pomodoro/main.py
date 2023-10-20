b6391892e018a034b6e630dad47e271472d75665
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-20 13:37:40 +0530

    code formatted to black

--- 100DaysofCode/Intermediate/Day-28-Tkinter,Dynamic Typing/Pomodoro/main.py
+++ 100DaysofCode/Intermediate/Day-28-Tkinter,Dynamic Typing/Pomodoro/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,33 +11,49 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(11)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def countdown(count):
+    """
+    Display the timer in 00:00 format
+    """
+    count_min = math.floor(count / 60)
+    count_seconds = count % 60
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_seconds}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro App")
-window.config(padx=100,pady=50,bg=YELLOW)
-canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
+window.config(padx=100, pady=50, bg=YELLOW)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
-heading_label = Label(text="Timer",bg=YELLOW,fg=GREEN,font=(FONT_NAME,35,"bold"))
-heading_label.grid(column=2,row=1)
+heading_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+heading_label.grid(column=2, row=1)
 
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.grid(column=2,row=2)
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=2, row=2)
 
-start_button = Button(text="Start",highlightthickness=0)
-start_button.grid(column=1,row=3)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=1, row=3)
 
-stop_button = Button(text="Stop",highlightthickness=0)
-stop_button.grid(column=3,row=3)
+stop_button = Button(text="Stop", highlightthickness=0)
+stop_button.grid(column=3, row=3)
 
-check_mark = Label(text="✔",bg=YELLOW,fg=GREEN,font=(FONT_NAME,35,"bold"))
-check_mark.grid(column=2,row=4)
+check_mark = Label(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 35, "bold"))
+check_mark.grid(column=2, row=4)
 
 window.mainloop()
