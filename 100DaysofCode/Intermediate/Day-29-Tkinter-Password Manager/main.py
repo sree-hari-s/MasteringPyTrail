e5b55db1f6dff1b95e2711a30bb2a1be6efb9e54
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-24 16:31:25 +0530

    feat - generate password functionality implemented

--- 100DaysofCode/Intermediate/Day-29-Tkinter-Password Manager/main.py
+++ 100DaysofCode/Intermediate/Day-29-Tkinter-Password Manager/main.py
@@ -1,7 +1,23 @@
 from tkinter import *
+from random import randint,choice,shuffle
 
 FONT_DEFAULT = ("Arial", 12)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    password_input.delete(0,'end')
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    
+    password_letters = [choice(letters) for _ in range(randint(8,10))]
+    password_numbers = [choice(numbers) for _ in range(randint(2,4))]
+    password_symbols = [choice(symbols) for _ in range(randint(2,4))]
+
+    password_list=password_letters + password_numbers + password_symbols
+    shuffle(password_list)
+    
+    password = "".join(password_list)
+    password_input.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -38,7 +54,7 @@ password_input = Entry(width=26, font=FONT_DEFAULT)
 password_input.grid(row=3, column=1)
 
 # Buttons
-generate_button = Button(text="Generate", width=10)
+generate_button = Button(text="Generate", width=10,command = generate_password)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=46)
