3481267d932a4e23ccdbec874c9b32bc6c1d6b34
Purna Shrestha <crush9918@gmail.com>
2023-10-28 10:38:02 +0545

    Add an ASCII art for displaying the remaining lives

--- Projects/Hangman/hangman.py
+++ Projects/Hangman/hangman.py
@@ -1,8 +1,57 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''']
+
 word_list = [
     {"word": "python", "hint": "A popular programming language"},
-    {"word": "java", "hint": "Another widely used programming language"},
+    {"word": "javascript", "hint": "Another widely used programming language"},
     {"word": "programming", "hint": "The act of writing code"},
     {"word": "hangman", "hint": "The name of this game"},
     {"word": "computer", "hint": "An electronic device for processing data"},
@@ -16,41 +65,36 @@ def choose_word():
 def play_hangman(word, hint):
     word = word.lower()
     guessed_letters = set()
-    attempts = 6
+    attempts = 5
 
     print("Welcome to Hangman!")
-    print(f"Hint: {hint}")
-    print("Try to guess the word. You have 6 attempts.")
+    print("Hint: " + hint)
+    print("Try to guess the word. You have 5 attempts.")
 
     while attempts > 0:
-        display_word = ""
-        for letter in word:
-            if letter in guessed_letters:
-                display_word += letter
-            else:
-                display_word += "_"
+        display_word = "".join([letter if letter in guessed_letters else "_" for letter in word])
 
+        print(stages[5 - attempts])
         print("Word: " + display_word)
         print("Guessed letters: " + ", ".join(guessed_letters))
         print(f"Attempts left: {attempts}")
+
+        if set(word) == guessed_letters:
+            print(f"Congratulations! You guessed the word: {word}")
+            break
 
         guess = input("Guess a letter: ").lower()
 
         if guess in guessed_letters:
             print("You've already guessed that letter.")
-        elif guess in word:
-            guessed_letters.add(guess)
         else:
             guessed_letters.add(guess)
-            attempts -= 1
+            if guess not in word:
+                attempts -= 1
 
-        if set(word) == guessed_letters:
-            print("Congratulations! You guessed the word: " + word)
-            break
-
-    if attempts == 0:
+    if attempts == 0 and set(word) != guessed_letters:
         print(f"Out of attempts! The word was '{word}'.")
-
+        print(stages[5]) 
 if __name__ == "__main__":
     chosen_word = choose_word()
     play_hangman(chosen_word["word"], chosen_word["hint"])
