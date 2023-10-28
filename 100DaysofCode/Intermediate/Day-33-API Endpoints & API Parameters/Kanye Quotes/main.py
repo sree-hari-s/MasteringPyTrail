d1c0a3c845fd8032325f4371d47ae5656ff60966
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-29 00:28:57 +0530

    feat- kanye quote generator completed

--- 100DaysofCode/Intermediate/Day-33-API Endpoints & API Parameters/Kanye Quotes/main.py
+++ 100DaysofCode/Intermediate/Day-33-API Endpoints & API Parameters/Kanye Quotes/main.py
@@ -1,10 +1,11 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
+    response = requests.get("https://api.kanye.rest/")
+    data = response.json()
+    canvas.itemconfig(quote_text, text=f"{data['quote']}")
 
 
 window = Tk()
@@ -14,13 +15,19 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(
+    150,
+    207,
+    text="Kanye Quote Goes HERE",
+    width=250,
+    font=("Arial", 30, "bold"),
+    fill="white",
+)
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
 
 
 window.mainloop()
