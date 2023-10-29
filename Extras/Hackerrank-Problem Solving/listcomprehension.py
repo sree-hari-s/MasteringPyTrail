a5f988711e1dc43828bd422946c1177462e90212
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-10-30 00:34:04 +0530

    fix - link to problem added

--- Extras/Hackerrank-Problem Solving/listcomprehension.py
+++ Extras/Hackerrank-Problem Solving/listcomprehension.py
@@ -1,4 +1,7 @@
-#list comprehension hackerrank
+"""
+# List Comprehension Hackerrank
+https://www.hackerrank.com/challenges/list-comprehensions/problem
+"""
 ls = [[i, n, w]for i in range(x+1) for n in range(y+1) for w in range(z+1)]
 new_ls = [i if i[0]+i[1]+i[2] != n else "" for i in ls]
 new_ls = [i for i in new_ls if i != ""]
