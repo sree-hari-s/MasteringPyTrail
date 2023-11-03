dc443efcbcdb5bf32df6f07a1f090cdc5b5593e5
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-03 12:41:41 +0530

    feat - question data from Open Trivia API

--- 100DaysofCode/Intermediate/Day-34-API Practice/Quizzler-App/data.py
+++ 100DaysofCode/Intermediate/Day-34-API Practice/Quizzler-App/data.py
@@ -1,4 +1,16 @@
-question_data = [
+import requests
+
+parameters = {
+    "amount" : 10,
+    "type" : "boolean"
+}
+
+try:
+    response = requests.get('https://opentdb.com/api.php',params=parameters)
+    data = response.json()
+    question_data = data['results']
+except:
+    question_data = [
     {
         "category": "Science: Computers",
         "type": "boolean",
