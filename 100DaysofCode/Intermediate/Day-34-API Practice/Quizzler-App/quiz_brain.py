993b26ebefa8fb13ef417f9efb53f317b12f69e5
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-03 12:42:16 +0530

    fix - Unescaping html enitites from question

--- 100DaysofCode/Intermediate/Day-34-API Practice/Quizzler-App/quiz_brain.py
+++ 100DaysofCode/Intermediate/Day-34-API Practice/Quizzler-App/quiz_brain.py
@@ -1,3 +1,5 @@
+import html
+
 class QuizBrain:
 
     def __init__(self, q_list):
@@ -12,7 +14,8 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
+        question_text = html.unescape(self.current_question.text)
+        user_answer = input(f"Q.{self.question_number}: {question_text} (True/False): ")
         self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
