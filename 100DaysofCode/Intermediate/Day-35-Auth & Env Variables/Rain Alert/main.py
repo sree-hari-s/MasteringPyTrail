ff892ea1e3a2a0a62f2c31e8c5ad3140e7a69f8d
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-04 00:54:37 +0530

    feat - rain alert application completed

---
+++ 100DaysofCode/Intermediate/Day-35-Auth & Env Variables/Rain Alert/main.py
@@ -0,0 +1,41 @@
+import os
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MY_LATITUDE = 8.528861
+MY_LONGITUDE = 76.913048
+
+parameters ={
+"lat": MY_LATITUDE,
+"lon": MY_LONGITUDE,
+"appid": os.environ['api_key'],
+"exclude":"current,minutely,daily,alerts"
+}
+OWM_endpoint = "https://api.openweathermap.org/data/2.8/onecall"
+response = requests.get(OWM_endpoint,params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+
+weather_12hr = weather_data['hourly'][:12] # Slice through the dictionary to get required data to a list
+will_rain = False
+for hour_data in weather_12hr:
+    weather_code = hour_data['weather'][0]['id']
+    if int(weather_code)<700:
+        will_rain=True
+
+if will_rain:
+    msg = "Bring an Umbrella"
+
+    client = Client(os.environ["account_sid"], os.environ["auth_token"])
+
+    message = client.messages \
+        .create(
+            from_='+12054303600',
+            body=msg,
+            to='+918089776183'
+        )
+
+    print(message.status)
