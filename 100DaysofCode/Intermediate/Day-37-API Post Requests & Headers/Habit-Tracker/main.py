b1b9c2f27db2867c56949aadb3f5ffa2d052a384
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-12 00:27:47 +0530

    feat - modified to add specific dates manually

--- 100DaysofCode/Intermediate/Day-37-API Post Requests & Headers/Habit-Tracker/main.py
+++ 100DaysofCode/Intermediate/Day-37-API Post Requests & Headers/Habit-Tracker/main.py
@@ -5,6 +5,7 @@ pixels_endpoint = "https://pixe.la/v1/users"
 
 TOKEN = "fasjkhfalksjhffas"
 USERNAME = "sreeharis"
+GRAPH_ID ="graph1"
 
 user_params = {
     "token": TOKEN,
@@ -19,7 +20,7 @@ print(response.text)
 graph_endpoint = f"{pixels_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id":"graph1",
+    "id":GRAPH_ID,
     "name":"Step Counter Graph",
     "unit" : "Km",
     "type" : "float",
@@ -33,3 +34,17 @@ headers = {
 response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
 print(response.text)
 
+graph_data_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+# today = str(dt.datetime.now().date()).replace('-','')  # using split method to get todays date
+today = dt.datetime.now().strftime("%Y%m%d") # Get todays date using strftime method
+# yesterday = dt.datetime(year=2023,month=11,day=10).strftime("%Y%m%d")
+print(today)
+
+graph_data = {
+    "date":today,
+    "quantity":"10"
+}
+
+response = requests.post(url=graph_data_endpoint,json=graph_data,headers=headers)
+print(response.text)
