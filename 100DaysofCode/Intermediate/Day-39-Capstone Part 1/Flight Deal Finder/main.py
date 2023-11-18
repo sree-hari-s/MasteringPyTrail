965e1c0ef99c7e98ab2e01805fd76fd66dda9a1c
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-18 12:38:46 +0530

    feat - step 4 completed
    send notification via sms if flight price lower than in google sheet

--- 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/main.py
+++ 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/main.py
@@ -1,26 +1,35 @@
 from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
-
-data_manager = DataManager()
-sheet_data = data_manager.get_destination_data()
-flight_search = FlightSearch()
+from notification_manager import NotificationManager
 
 ORIGIN_CITY_IATA = "LON"
 
-if sheet_data[0]["iataCode"] == "":
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+data_manager = DataManager()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
 
-tomorrow = datetime.now() + timedelta(days=1)
-six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+sheet_data = data_manager.get_destination_data()
+
+if sheet_data[0]["iataCode"] == "":
+    city_names = [row["city"] for row in sheet_data]
+    print(city_names)
+    codes = flight_search.get_destination_codes(city_names)
+    data_manager.update_destination_codes(codes)
+    sheet_data = data_manager.get_destination_data()
+
+today = datetime.now() + timedelta(1)
+six_month_from_today = datetime.now() + timedelta(6 * 30)
 
 for destination in sheet_data:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
         destination["iataCode"],
-        from_time=tomorrow,
+        from_time=today,
         to_time=six_month_from_today
     )
+
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_sms(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
--- 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/notification_manager.py
+++ 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/notification_manager.py
@@ -1,3 +1,20 @@
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(os.environ['TWILIO_SID'], os.environ['TWILIO_AUTH_TOKEN'])
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=os.environ['TWILIO_VIRTUAL_NUMBER'],
+            to=os.environ['TWILIO_VERIFIED_NUMBER'],
+        )
+        # Prints if successfully sent.
+        print(message.sid)
