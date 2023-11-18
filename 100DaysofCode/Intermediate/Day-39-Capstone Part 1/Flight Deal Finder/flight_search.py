a05108b76971ae8e673134ab720ef25d00dd4daf
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-18 12:33:11 +0530

    feat - step 4 completed
    search for cheap flights

--- 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/flight_search.py
+++ 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/flight_search.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from flight_data import *
 
 load_dotenv()
 
@@ -17,3 +18,42 @@ class FlightSearch:
         results = response.json()["locations"]
         code = results[0]["code"]
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": os.environ['TEQUILA_API_KEY']}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+
+        response = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/v2/search",
+            headers=headers,
+            params=query,
+        )
+        
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
--- 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/main.py
+++ 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/main.py
@@ -1,21 +1,26 @@
-# 4. Pass the data back to the main.py file, so that you can print the data from main.py
+from datetime import datetime, timedelta
 from data_manager import DataManager
+from flight_search import FlightSearch
+
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-# print(sheet_data)
+flight_search = FlightSearch()
 
-#  5. In main.py check if sheet_data contains any values for the "iataCode" key.
-#  If not, then the IATA Codes column is empty in the Google Sheet.
-#  In this case, pass each city name in sheet_data one-by-one
-#  to the FlightSearch class to get the corresponding IATA code
-#  for that city using the Flight Search API.
-#  You should use the code you get back to update the sheet_data dictionary.
+ORIGIN_CITY_IATA = "LON"
+
 if sheet_data[0]["iataCode"] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
