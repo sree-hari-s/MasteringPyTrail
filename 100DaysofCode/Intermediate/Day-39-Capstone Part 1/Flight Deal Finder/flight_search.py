15e08ad501f2e17a57ce3e801688f338c52f110e
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-11-18 12:27:07 +0530

    feat - step 3 completed
    added iata codes to sheet

--- 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/flight_search.py
+++ 100DaysofCode/Intermediate/Day-39-Capstone Part 1/Flight Deal Finder/flight_search.py
@@ -10,6 +10,10 @@ TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 class FlightSearch:
 
     def get_destination_code(self, city_name):
-        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code = "TESTING"
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": os.environ['TEQUILA_API_KEY']}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         return code
