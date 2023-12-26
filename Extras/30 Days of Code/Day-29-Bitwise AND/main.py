e04c41f2e2b0488080fda514fa8c5c4f9d0071f7
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-12-26 19:39:15 +0530

    feat - day 28 29 completed

---
+++ Extras/30 Days of Code/Day-29-Bitwise AND/main.py
@@ -0,0 +1,24 @@
+import os
+
+
+def bitwiseAnd(N, K):
+    return K - 1 if ((K - 1) | K) <= N else K - 2
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + "\n")
+
+    fptr.close()
