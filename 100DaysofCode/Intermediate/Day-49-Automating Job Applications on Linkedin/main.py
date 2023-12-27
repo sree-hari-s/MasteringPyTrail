130eceaec0ad7495c1c959fc0e07216cc803723f
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-12-27 22:33:56 +0530

    feat - login automated

--- 100DaysofCode/Intermediate/Day-49-Automating Job Applications on Linkedin/main.py
+++ 100DaysofCode/Intermediate/Day-49-Automating Job Applications on Linkedin/main.py
@@ -14,7 +14,20 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-email = os.environ["EMAIL"]
-password = os.environ["PASSWORD"]
+ACCOUNT_EMAIL = os.environ["EMAIL"]
+ACCOUNT_PASSWORD = os.environ["PASSWORD"]
 
 driver.get(url=URL)
+
+# Click Sign in Button
+time.sleep(2)
+sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
+sign_in_button.click()
+
+# Sign in
+time.sleep(5)
+email_field = driver.find_element(by=By.ID, value="username")
+email_field.send_keys(ACCOUNT_EMAIL)
+password_field = driver.find_element(by=By.ID, value="password")
+password_field.send_keys(ACCOUNT_PASSWORD)
+password_field.send_keys(Keys.ENTER)
