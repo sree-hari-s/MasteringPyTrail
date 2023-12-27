8f604e33f03db4395de4e926665d5fe0db8ab57b
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-12-27 22:54:24 +0530

    feat - automate button clicking
    save and submit button click

--- 100DaysofCode/Intermediate/Day-49-Automating Job Applications on Linkedin/main.py
+++ 100DaysofCode/Intermediate/Day-49-Automating Job Applications on Linkedin/main.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
+from selenium.common.exceptions import NoSuchElementException
 
 load_dotenv()
 
@@ -20,9 +21,12 @@ ACCOUNT_PASSWORD = os.environ["PASSWORD"]
 driver.get(url=URL)
 
 # Click Sign in Button
-time.sleep(2)
-sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
-sign_in_button.click()
+try:
+    time.sleep(2)
+    sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
+    sign_in_button.click()
+except NoSuchElementException:
+    print("No such element")
 
 # Sign in
 time.sleep(5)
@@ -31,3 +35,17 @@ email_field.send_keys(ACCOUNT_EMAIL)
 password_field = driver.find_element(by=By.ID, value="password")
 password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
+
+#Locate Save button
+time.sleep(5)
+save_button = driver.find_element(By.CSS_SELECTOR,value=".jobs-save-button")
+save_button.click()
+
+#Locate the apply button
+time.sleep(5)
+apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+apply_button.click()
+
+#Submit the application
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+submit_button.click()
