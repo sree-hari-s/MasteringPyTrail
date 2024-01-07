9946f192c58c59c1366f48cf189e371de25b52ea
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-07 22:40:56 +0530

    fix - fixed code implementation

--- Extras/Hackerrank-Problem Solving/listcomprehension.py
+++ Extras/Hackerrank-Problem Solving/listcomprehension.py
@@ -2,7 +2,13 @@
 # List Comprehension Hackerrank
 https://www.hackerrank.com/challenges/list-comprehensions/problem
 """
-ls = [[i, n, w]for i in range(x+1) for n in range(y+1) for w in range(z+1)]
-new_ls = [i if i[0]+i[1]+i[2] != n else "" for i in ls]
+if __name__ == "__main__":
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+ls = [[i, n, w] for i in range(x + 1) for n in range(y + 1) for w in range(z + 1)]
+new_ls = [i if i[0] + i[1] + i[2] != n else "" for i in ls]
 new_ls = [i for i in new_ls if i != ""]
 print(new_ls)
