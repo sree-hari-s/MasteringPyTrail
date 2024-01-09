59e83598872225483b1b0d81ff1e6d8ee157711c
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-09 23:23:33 +0530

    feat - simple blog website created using flask

--- 100DaysofCode/Intermediate/Day-57-Templating with Jinja in Flask Application/Blog-templating/main.py
+++ 100DaysofCode/Intermediate/Day-57-Templating with Jinja in Flask Application/Blog-templating/main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+from post import Post
+import requests
 
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",all_posts=post_objects)
+
+@app.route('/post/<int:id>')
+def show_post(id):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == id:
+            requested_post = blog_post
+    return render_template("post.html",post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 100DaysofCode/Intermediate/Day-57-Templating with Jinja in Flask Application/Blog-templating/post.py
+++ 100DaysofCode/Intermediate/Day-57-Templating with Jinja in Flask Application/Blog-templating/post.py
@@ -1,2 +1,6 @@
 class Post:
-    pass
+    def __init__(self,post_id,title,subtitle,body):
+        self.id = post_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
