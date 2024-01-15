47d94435c22ca33a0f75e05b809dc9c9c7fd4668
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-15 13:15:41 +0530

    feat - updated to contact by email

--- 100DaysofCode/Advanced/Day-59-Capstone Project - Blog/main.py
+++ 100DaysofCode/Advanced/Day-59-Capstone Project - Blog/main.py
@@ -1,6 +1,13 @@
+import os
 import requests
-from flask import Flask,render_template
+import smtplib
+from flask import Flask,render_template,request
 from post import Post
+from dotenv import load_dotenv
+
+load_dotenv()
+OWN_EMAIL=os.environ['EMAIL']
+OWN_PASSWORD=os.environ['PASSWORD']
 
 response = requests.get('https://api.npoint.io/79af83afe03463a29f67')
 data = response.json()
@@ -27,9 +34,24 @@ def post(id):
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact',methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        data = request.form
+        name = data.get("name")
+        email = data.get("email")
+        phone = data.get("phone")
+        message = data.get("message")
+        send_email(name,email,phone,message)
+        return render_template('contact.html',msg_sent = True)
     return render_template('contact.html')
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com", port="587") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
