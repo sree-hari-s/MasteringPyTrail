6b1f6cf07684ef70ece09a95e49108b32be82ab6
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-16 23:10:04 +0530

    feat - validators added

--- 100DaysofCode/Advanced/Day-61-Flask-WTForms/main.py
+++ 100DaysofCode/Advanced/Day-61-Flask-WTForms/main.py
@@ -2,12 +2,12 @@ import os
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField 
-
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField ('Password')
-    submit = SubmitField("Login")
+    email = StringField(label='Email',validators=[DataRequired()])
+    password = PasswordField (label='Password',validators=[DataRequired()])
+    submit = SubmitField(label="Login")
     
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -19,7 +19,12 @@ def home():
 @app.route("/login",methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
-    return render_template('login.html', form=login_form)
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
