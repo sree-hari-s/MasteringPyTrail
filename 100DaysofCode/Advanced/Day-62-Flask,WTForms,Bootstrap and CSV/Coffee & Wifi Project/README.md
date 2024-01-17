95b2e3d5f2bd7c7b9c8a89bf3e8b7464987c7384
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-17 20:06:11 +0530

    feat - coffee and wifi project completed

---
+++ 100DaysofCode/Advanced/Day-62-Flask,WTForms,Bootstrap and CSV/Coffee & Wifi Project/README.md
@@ -0,0 +1,1 @@
+# Coffee and Wifi
--- 100DaysofCode/Advanced/Day-62-Flask,WTForms,Bootstrap and CSV/Coffee & Wifi Project/main.py
+++ 100DaysofCode/Advanced/Day-62-Flask,WTForms,Bootstrap and CSV/Coffee & Wifi Project/main.py
@@ -1,27 +1,38 @@
-from flask import Flask, render_template
+import csv
+import os
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-import csv
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = os.urandom(24)
 Bootstrap5(app)
 
-
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
+    cafe = StringField("Cafe name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    location = StringField("Cafe Location Map URL", validators=[DataRequired(), URL()])
+    open_time = StringField("Opening Time e.g. 8AM", validators=[DataRequired()])
+    closing_time = StringField("Closing Time e.g. 5:30PM", validators=[DataRequired()])
+    coffee_rating = SelectField(
+        "Coffee Rating",
+        choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"],
+        validators=[DataRequired()],
+    )
+    wifi_rating = SelectField(
+        "Wifi Strength Rating",
+        choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"],
+        validators=[DataRequired()],
+    )
+    power_rating = SelectField(
+        "Power Socket Availability",
+        choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"],
+        validators=[DataRequired()],
+    )
+    submit = SubmitField("Submit")
 
 
 # all Flask routes below
@@ -30,26 +41,33 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route("/add", methods=["GET", "POST"])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
-    return render_template('add.html', form=form)
+        with open("cafe-data.csv", mode="a", encoding="utf-8") as csv_file:
+            csv_file.write(
+                f"\n{form.cafe.data},"
+                f"{form.location.data},"
+                f"{form.open_time.data},"
+                f"{form.closing_time.data},"
+                f"{form.coffee_rating.data},"
+                f"{form.wifi_rating.data},"
+                f"{form.power_rating.data}"
+            )
+        return redirect(url_for("cafes"))
+    return render_template("add.html", form=form)
 
 
-@app.route('/cafes')
+@app.route("/cafes")
 def cafes():
-    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
+    with open("cafe-data.csv", newline="", encoding="utf-8") as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=",")
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    return render_template("cafes.html", cafes=list_of_rows)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
