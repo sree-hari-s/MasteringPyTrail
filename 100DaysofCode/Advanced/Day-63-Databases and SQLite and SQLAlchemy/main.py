f7194fdb324ddc707d1356d63265c9500bd58180
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-18 22:22:17 +0530

    feat - day 63 starting code added

---
+++ 100DaysofCode/Advanced/Day-63-Databases and SQLite and SQLAlchemy/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/add")
+def add():
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
