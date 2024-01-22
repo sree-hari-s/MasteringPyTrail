1f2b55757c81b9e085fa2bbca11a6dbf41be37e4
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-22 20:20:24 +0530

    feat - view movie list items

--- 100DaysofCode/Advanced/Day-64-Top 10 Movies Website/main.py
+++ 100DaysofCode/Advanced/Day-64-Top 10 Movies Website/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -8,32 +9,34 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.urandom(24)
 Bootstrap5(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+db = SQLAlchemy(app)
+
 # CREATE DB
-
-
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255),unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Float,nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(255), nullable=True)
+    img_url = db.Column(db.String(255), nullable=False)
 # CREATE TABLE
-
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movie_data = db.session.execute(db.select(Movie).order_by(Movie.title))
+    all_movies = list(movie_data.scalars())
+    for movie in all_movies:
+        print(f"Name:{movie.title}")
+    return render_template("index.html",movies=all_movies)
 
 
 if __name__ == '__main__':
