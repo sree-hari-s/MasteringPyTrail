58ab9245be5b590339da20ad8c3e2915788ce456
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-22 23:21:21 +0530

    fix - unnecessary imports removed

--- 100DaysofCode/Advanced/Day-64-Top 10 Movies Website/main.py
+++ 100DaysofCode/Advanced/Day-64-Top 10 Movies Website/main.py
@@ -2,8 +2,6 @@ import os
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
