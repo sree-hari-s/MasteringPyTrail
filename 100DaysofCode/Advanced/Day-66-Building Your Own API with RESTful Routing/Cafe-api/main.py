0fe29075ea9c1f1c951b3414984937cce991e71b
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-24 19:46:13 +0530

    feat - get random cafe

--- 100DaysofCode/Advanced/Day-66-Building Your Own API with RESTful Routing/Cafe-api/main.py
+++ 100DaysofCode/Advanced/Day-66-Building Your Own API with RESTful Routing/Cafe-api/main.py
@@ -1,20 +1,9 @@
+import random
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
 
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 
@@ -41,6 +30,14 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self):
+        dictionary = {}
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self,column.name)
+        return dictionary
+
+        #Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 with app.app_context():
     db.create_all()
@@ -50,7 +47,30 @@ with app.app_context():
 def home():
     return render_template("index.html")
 
-
+@app.route("/random")
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    """This way is more flexible, it allows you to have perfect control over the JSON response"""
+    # return jsonify(cafe={
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+        
+    #     #Put some properties in a sub-category
+    #     "amenities": {
+    #       "seats": random_cafe.seats,
+    #       "has_toilet": random_cafe.has_toilet,
+    #       "has_wifi": random_cafe.has_wifi,
+    #       "has_sockets": random_cafe.has_sockets,
+    #       "can_take_calls": random_cafe.can_take_calls,
+    #       "coffee_price": random_cafe.coffee_price,
+    #     }
+    # })
+    return jsonify(cafe=random_cafe.to_dict())
 # HTTP GET - Read Record
 
 # HTTP POST - Create Record
