48f58d69238665c70def303578ab8e9b67afe666
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-25 22:36:08 +0530

    feat - GET request
    all blog posts retrieval
    get individual posts

--- 100DaysofCode/Advanced/Day-67-Capstone Project-RESTful Routing/main.py
+++ 100DaysofCode/Advanced/Day-67-Capstone Project-RESTful Routing/main.py
@@ -39,15 +39,16 @@ with app.app_context():
 
 @app.route('/')
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
+    # Query the database for all the posts. Convert the data to a python list.
     posts = []
+    all_posts = db.session.execute(db.select(BlogPost))
+    posts = all_posts.scalars().all()
     return render_template("index.html", all_posts=posts)
 
-# TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
-    # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    # Retrieve a BlogPost from the database based on the post_id
+    requested_post = db.get_or_404(BlogPost,post_id)
     return render_template("post.html", post=requested_post)
 
 
