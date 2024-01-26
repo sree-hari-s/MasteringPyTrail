04834bef6f707ce273e03d9af82c11849dd3421e
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2024-01-26 23:27:12 +0530

    feat - Edit Existing Blog Posts

--- 100DaysofCode/Advanced/Day-67-Capstone Project-RESTful Routing/main.py
+++ 100DaysofCode/Advanced/Day-67-Capstone Project-RESTful Routing/main.py
@@ -76,8 +76,26 @@ def add_new_post():
         db.session.commit()
         return redirect(url_for('get_all_posts'))
     return render_template("make-post.html",form=form)
-# TODO: edit_post() to change an existing blog post
 
+@app.route('/edit-post/<int:post_id>',methods=['GET','POST'])
+def edit_post(post_id):
+    post = db.get_or_404(BlogPost,post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data    
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html",form = edit_form,is_edit=True)
 # TODO: delete_post() to remove a blog post from the database
 
 # Below is the code from previous lessons. No changes needed.
